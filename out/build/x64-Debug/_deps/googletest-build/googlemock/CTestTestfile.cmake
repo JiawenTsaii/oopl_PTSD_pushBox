f09a38a7b3efdf95b92ce401a4f91ab9899ed0c1
ZYuC <71123702+ZYuC-2002@users.noreply.github.com>
2025-03-09 23:01:36 +0800

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/1School/OOPL/z_trytry/oopl_PTSD_pushBox/PTSD/lib/googletest/googlemock
-# Build directory: E:/1School/OOPL/z_trytry/oopl_PTSD_pushBox/out/build/x64-Debug/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- out/build/x64-Debug/_deps/ptsd-build/Tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "E:/1School/OOPL/z_trytry/oopl_PTSD_pushBox/out/build/x64-Debug/_deps/ptsd-build/Tests[1]_tests.cmake")
-  include("E:/1School/OOPL/z_trytry/oopl_PTSD_pushBox/out/build/x64-Debug/_deps/ptsd-build/Tests[1]_tests.cmake")
-else()
-  add_test(Tests_NOT_BUILT Tests_NOT_BUILT)
-endif()
