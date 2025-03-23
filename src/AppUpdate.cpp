33a5e8899cc1383a6fd58eb31afec669178129d3
ZYuC <yuchiehzhuang2002@gmail.com>
2025-03-23 21:54:44 +0800

    0323 人物移動(地圖更新)

--- src/AppUpdate.cpp
+++ src/AppUpdate.cpp
@@ -7,50 +7,103 @@
 
 void App::Update() {
 
-    if (m_Phase == Phase::LEVEL1 || m_Phase == Phase::LEVEL2) {
+    static bool keyProcessed = false;
+
+    bool needUpdate = false;
+
+    if (m_Phase != Phase::MENU && m_Phase != Phase::LEVEL) {
         if (m_Player) {
-			//std::cout << "Player" << std::endl;
             m_Player->SetVisible(true);
         }
         if (m_Box) {
-            //std::cout << "Box" << std::endl;
             m_Box->SetVisible(true);
         }
         for (auto& wall : m_Wall) {
-            //std::cout << "Walls" << std::endl;
             wall->SetVisible(true);
         }
-		for (auto& floor : m_Floor) {
-            //std::cout << "Floors" << std::endl;
-			floor->SetVisible(true);
-		}
+        for (auto& floor : m_Floor) {
+            floor->SetVisible(true);
+        }
     }
 
-    /* 動畫 */
-    /* 如果上面有箱子就人和箱子一起動，否則人動 */
-    /* 看一格的大小決定每次移動多少 */
-    //m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 5 });
-    //if (Util::Input::IsKeyPressed(Util::Keycode::UP)) {
-    //    if (m_Player->IfCollides(m_Box)) {
-    //        // m_Player和m_Box一起動
-    //        for (int i = 0; i < 80; i++) {
-    //            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 1 });
-    //            m_Box->SetPosition({ m_Box->GetPosition().x, m_Box->GetPosition().y + 1 });
-    //        }
-    //    }
-    //    else {
-    //        for (int i = 0; i < 80; i++) {
-    //            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 1 });
-    //        }
-    //        // m_Player移動一個箱子的距離
-    //            // for迴圈，每次移動0.5或1，中間要有休息時間，才會像動畫
-    //    }
-    //}
-    if (Util::Input::IsKeyPressed(Util::Keycode::DOWN)) {
+    if (Util::Input::IsKeyPressed(Util::Keycode::UP)) {
+        if (!keyProcessed) {
+            for (int i = 7; i >= 0; i--) {
+                for (int j = 0; j < 6; j++) {
+                    if (m_GameMap[i][j] == 4) {
+                        if (i > 0 && m_GameMap[i - 1][j] == 2) { // 確保不超出邊界並且上面是空地
+                            m_GameMap[i][j] = 2; // 將當前位置設為空地
+                            m_GameMap[i - 1][j] = 4; // 將上面一格設為玩家
+                            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 60 });
+                            needUpdate = true;
+                        }
+                        break; // 找到玩家並移動後直接跳出內層迴圈
+                    }
+                }
+                if (needUpdate) break; // 找到玩家並移動後直接跳出外層迴圈
+            }
+            keyProcessed = true;
+        }
     }
-    if (Util::Input::IsKeyPressed(Util::Keycode::LEFT)) {
+    else if (Util::Input::IsKeyPressed(Util::Keycode::DOWN)) {
+        if (!keyProcessed) {
+            for (int i = 7; i >= 0; i--) {
+                for (int j = 0; j < 6; j++) {
+                    if (m_GameMap[i][j] == 4) {
+                        if (i < 7 && m_GameMap[i + 1][j] == 2) { // 確保不超出邊界並且下面是空地
+                            m_GameMap[i][j] = 2; // 將當前位置設為空地
+                            m_GameMap[i + 1][j] = 4; // 將下面一格設為玩家
+                            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y - 60 });
+                            needUpdate = true;
+                        }
+                        break; // 找到玩家並移動後直接跳出內層迴圈
+                    }
+                }
+                if (needUpdate) break; // 找到玩家並移動後直接跳出外層迴圈
+            }
+            keyProcessed = true;
+        }
     }
-    if (Util::Input::IsKeyPressed(Util::Keycode::RIGHT)) {
+    else if (Util::Input::IsKeyPressed(Util::Keycode::LEFT)) {
+        if (!keyProcessed) {
+            for (int i = 7; i >= 0; i--) {
+                for (int j = 0; j < 6; j++) {
+                    if (m_GameMap[i][j] == 4) {
+                        if (j > 0 && m_GameMap[i][j - 1] == 2) { // 確保不超出邊界並且左邊是空地
+                            m_GameMap[i][j] = 2; // 將當前位置設為空地
+                            m_GameMap[i][j - 1] = 4; // 將左邊一格設為玩家
+                            m_Player->SetPosition({ m_Player->GetPosition().x - 60, m_Player->GetPosition().y });
+                            needUpdate = true;
+                        }
+                        break; // 找到玩家並移動後直接跳出內層迴圈
+                    }
+                }
+                if (needUpdate) break; // 找到玩家並移動後直接跳出外層迴圈
+            }
+            keyProcessed = true;
+        }
+    }
+    else if (Util::Input::IsKeyPressed(Util::Keycode::RIGHT)) {
+        if (!keyProcessed) {
+            for (int i = 7; i >= 0; i--) {
+                for (int j = 0; j < 6; j++) {
+                    if (m_GameMap[i][j] == 4) {
+                        if (j < 5 && m_GameMap[i][j + 1] == 2) { // 確保不超出邊界並且右邊是空地
+                            m_GameMap[i][j] = 2; // 將當前位置設為空地
+                            m_GameMap[i][j + 1] = 4; // 將右邊一格設為玩家
+                            m_Player->SetPosition({ m_Player->GetPosition().x + 60, m_Player->GetPosition().y });
+                            needUpdate = true;
+                        }
+                        break; // 找到玩家並移動後直接跳出內層迴圈
+                    }
+                }
+                if (needUpdate) break; // 找到玩家並移動後直接跳出外層迴圈
+            }
+            keyProcessed = true;
+        }
+    }
+    else {
+        keyProcessed = false; // 如果沒有按下任何方向鍵，重置 keyProcessed 標誌
     }
 
     if (Util::Input::IsKeyPressed(Util::Keycode::RETURN) && !m_PhaseChanged) {
--- src/InitializeMap.cpp
+++ src/InitializeMap.cpp
@@ -2,58 +2,8 @@
 
 #include <iostream>
 
-//void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
-//	std::cout << "InitializeMap()" << std::endl;
-//    for (int i = 7; i >= 0; i--) {
-//        for (int j = 0; j < 6; j++) {
-//            /* wall: 60*60 */
-//            if (GameMap[i][j] == 1) {
-//                auto wall = std::make_shared<Character>(RESOURCE_DIR"/Object/wall.png");
-//                wall->SetZIndex(50);
-//                wall->SetPosition({ -150 + 60 * j, -210 + 60 * i });
-//                wall->SetVisible(false);
-//                m_Wall.push_back(wall);
-//                m_Root.AddChild(wall);
-//            }
-//            /* floor: 60*60 */
-//            else if (GameMap[i][j] == 2) {
-//                auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
-//                floor->SetZIndex(50);
-//                floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
-//                floor->SetVisible(false);
-//                m_Floor.push_back(floor);
-//                m_Root.AddChild(floor);
-//            }
-//            /* box: 60*60 */
-//            else if (GameMap[i][j] == 3) {
-//                m_Box = std::make_shared<Character>(RESOURCE_DIR"/Object/box.png");
-//                m_Box->SetPosition({ -150 + 60 * j, 210 - 60 * i });
-//                m_Box->SetVisible(false);
-//                m_Box->SetZIndex(50);
-//                m_Root.AddChild(m_Box);
-//            }
-//            /* player: 高度60 */
-//            else if (GameMap[i][j] == 4) {
-//                m_Player = std::make_shared<Character>(RESOURCE_DIR"/Object/player_down.png");
-//                m_Player->SetPosition({ -150 + 60 * j, 210 - 60 * i });
-//                m_Player->SetVisible(false);
-//                m_Player->SetZIndex(50);
-//                m_Root.AddChild(m_Player);
-//            }
-//            // 還是要用這種寫法呢?
-//            /*for (int i = 0; i < 3; ++i) {
-//                m_Doors.push_back(std::make_shared<Character>(GA_RESOURCE_DIR"/Image/Character/door_close.png"));
-//                m_Doors[i]->SetZIndex(5);
-//                m_Doors[i]->SetPosition({ 205.5f - 80.f * i, -3.5f });
-//                m_Doors[i]->SetVisible(false);
-//                m_Root.AddChild(m_Doors[i]);
-//            }*/
-//        }
-//    }
-//}
-
 void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
-    // 清空現有的地圖物件並從渲染樹中移除
+    // 清空現有的地圖物件
     for (auto& wall : m_Wall) {
         m_Root.RemoveChild(wall);
     }
@@ -74,50 +24,65 @@ void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
         m_Box.reset();
     }
 
+    /* 複製 GameMap 到 m_GameMap */
+	m_GameMap.assign(GameMap.begin(), GameMap.end());
+
+    // 原點在中間，所以左下角是(-180, -320)
+    // 640-480=160，160/2=80，所以上下各空80
+    // 第一個(左下角)箱子: (-150, -210)
+    // SetPosition是設定中間的位置
+
     for (int i = 7; i >= 0; i--) {
         for (int j = 0; j < 6; j++) {
             int tile = GameMap[i][j];
             switch (tile) {
-            case 1: // 牆壁
-            {
-                auto wall = std::make_shared<Character>(RESOURCE_DIR"/Object/wall.png");
-                wall->SetZIndex(50);
-                wall->SetPosition({ -150 + 60 * j, -210 + 60 * i });
-                wall->SetVisible(false);
-                m_Wall.push_back(wall);
-                m_Root.AddChild(wall);
-            }
-            break;
-            case 2: // 空地
-            {
-                auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
-                floor->SetZIndex(50);
-                floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
-                floor->SetVisible(false);
-                m_Floor.push_back(floor);
-                m_Root.AddChild(floor);
-            }
-            break;
-            case 3: // 箱子
-            {
-                m_Box = std::make_shared<Character>(RESOURCE_DIR"/Object/box.png");
-                m_Box->SetPosition({ -150 + 60 * j, 210 - 60 * i });
-                m_Box->SetVisible(false);
-                m_Box->SetZIndex(50);
-                m_Root.AddChild(m_Box);
-            }
-            break;
-            case 4: // 人物
-            {
-                m_Player = std::make_shared<Character>(RESOURCE_DIR"/Object/player_floor.png");
-                m_Player->SetPosition({ -150 + 60 * j, 210 - 60 * i });
-                m_Player->SetVisible(false);
-                m_Player->SetZIndex(50);
-                m_Root.AddChild(m_Player);
-            }
-            break;
-            default:
-                break;
+                case 1: // 牆壁
+                {
+                    auto wall = std::make_shared<Character>(RESOURCE_DIR"/Object/wall.png");
+                    wall->SetZIndex(50);
+                    wall->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    wall->SetVisible(false);
+                    m_Wall.push_back(wall);
+                    m_Root.AddChild(wall);
+                }
+                    break;
+                case 2: // 空地
+                {
+                    auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
+                    floor->SetZIndex(40);
+                    floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    floor->SetVisible(false);
+                    m_Floor.push_back(floor);
+                    m_Root.AddChild(floor);
+                }
+                    break;
+                case 3: // 箱子
+                {
+                    m_Box = std::make_shared<Character>(RESOURCE_DIR"/Object/box.png");
+                    m_Box->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    m_Box->SetVisible(false);
+                    m_Box->SetZIndex(50);
+                    m_Root.AddChild(m_Box);
+                }
+                    break;
+                case 4: // 人物
+                {
+                    auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
+                    floor->SetZIndex(40);
+                    floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    floor->SetVisible(false);
+                    m_Floor.push_back(floor);
+                    m_Root.AddChild(floor);
+
+                    m_Player = std::make_shared<Character>(RESOURCE_DIR"/Object/player_down.png");
+                    m_Player->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    m_Player->SetVisible(false);
+                    m_Player->SetZIndex(50);
+                    m_Root.AddChild(m_Player);
+                }
+                    break;
+                default:
+                    break;
             }
         }
     }
