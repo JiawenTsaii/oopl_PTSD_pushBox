321b400eb6aa2f8a07f9ff0fef09f7e21ba291a1
ZYuC <yuchiehzhuang2002@gmail.com>
2025-03-28 15:22:39 +0800

    0328 修改地圖(8*6->8*9)

--- src/AppUpdate.cpp
+++ src/AppUpdate.cpp
@@ -12,38 +12,42 @@ void App::Update() {
     bool needUpdate = false;
 
     if (m_Phase != Phase::MENU && m_Phase != Phase::LEVEL) {
-        if (m_Player) {
+        if (m_Player) { // 人物
             m_Player->SetVisible(true);
         }
-        if (m_Box) {
+		if (m_Box) { // 箱子
             m_Box->SetVisible(true);
         }
-        for (auto& wall : m_Wall) {
+		for (auto& wall : m_Wall) { // 牆壁
             wall->SetVisible(true);
         }
-        for (auto& floor : m_Floor) {
+		for (auto& floor : m_Floor) { // 地板
             floor->SetVisible(true);
         }
+		if (m_Point) { // 目標點
+			m_Point->SetVisible(true);
+		}
+        // 勾勾(m_Check)先不要顯示
     }
 
     if (Util::Input::IsKeyPressed(Util::Keycode::UP)) {
         if (!keyProcessed) {
             for (int i = 7; i >= 0; i--) {
-                for (int j = 0; j < 6; j++) {
+                for (int j = 0; j < 9; j++) {
                     if (m_GameMap[i][j] == 4) {
-                        if (i > 0 && m_GameMap[i - 1][j] == 2) { // 確保不超出邊界並且上面是空地
+                        if (m_GameMap[i - 1][j] == 2) { // 確保不超出邊界並且上面是空地
                             m_GameMap[i][j] = 2; // 將當前位置設為空地
                             m_GameMap[i - 1][j] = 4; // 將上面一格設為玩家
-                            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 60 });
+                            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 40 });
                             needUpdate = true;
                         }
-                        if (i > 0 && m_GameMap[i - 1][j] == 3) {
-							if (i > 1 && m_GameMap[i - 2][j] == 2) { // 確保箱子不會超出邊界並且箱子上面是空地
+                        if (m_GameMap[i - 1][j] == 3) {
+							if (m_GameMap[i - 2][j] == 2) { // 確保箱子不會超出邊界並且箱子上面是空地
 								m_GameMap[i][j] = 2; // 將當前位置設為空地
 								m_GameMap[i - 1][j] = 4; // 將上面一格設為玩家
 								m_GameMap[i - 2][j] = 3; // 將箱子上面一格設為箱子
-								m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 60 });
-								m_Box->SetPosition({ m_Box->GetPosition().x, m_Box->GetPosition().y + 60 });
+								m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y + 40 });
+								m_Box->SetPosition({ m_Box->GetPosition().x, m_Box->GetPosition().y + 40 });
 								needUpdate = true;
 							}
                         }
@@ -58,21 +62,21 @@ void App::Update() {
     else if (Util::Input::IsKeyPressed(Util::Keycode::DOWN)) {
         if (!keyProcessed) {
             for (int i = 7; i >= 0; i--) {
-                for (int j = 0; j < 6; j++) {
+                for (int j = 0; j < 9; j++) {
                     if (m_GameMap[i][j] == 4) {
-                        if (i < 7 && m_GameMap[i + 1][j] == 2) { // 確保不超出邊界並且下面是空地
+                        if (m_GameMap[i + 1][j] == 2) { // 確保不超出邊界並且下面是空地
                             m_GameMap[i][j] = 2; // 將當前位置設為空地
                             m_GameMap[i + 1][j] = 4; // 將下面一格設為玩家
-                            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y - 60 });
+                            m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y - 40 });
                             needUpdate = true;
                         }
-                        if (i < 7 && m_GameMap[i + 1][j] == 3) {
-                            if (i < 6 && m_GameMap[i + 2][j] == 2) { // 確保箱子不會超出邊界並且箱子下面是空地
+                        if (m_GameMap[i + 1][j] == 3) {
+                            if (m_GameMap[i + 2][j] == 2) { // 確保箱子不會超出邊界並且箱子下面是空地
                                 m_GameMap[i][j] = 2; // 將當前位置設為空地
                                 m_GameMap[i + 1][j] = 4; // 將下面一格設為玩家
                                 m_GameMap[i + 2][j] = 3; // 將箱子下面一格設為箱子
-                                m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y - 60 });
-                                m_Box->SetPosition({ m_Box->GetPosition().x, m_Box->GetPosition().y - 60 });
+                                m_Player->SetPosition({ m_Player->GetPosition().x, m_Player->GetPosition().y - 40 });
+                                m_Box->SetPosition({ m_Box->GetPosition().x, m_Box->GetPosition().y - 40 });
                                 needUpdate = true;
                             }
                         }
@@ -87,21 +91,21 @@ void App::Update() {
     else if (Util::Input::IsKeyPressed(Util::Keycode::LEFT)) {
         if (!keyProcessed) {
             for (int i = 7; i >= 0; i--) {
-                for (int j = 0; j < 6; j++) {
+                for (int j = 0; j < 9; j++) {
                     if (m_GameMap[i][j] == 4) {
-                        if (j > 0 && m_GameMap[i][j - 1] == 2) { // 確保不超出邊界並且左邊是空地
+                        if (m_GameMap[i][j - 1] == 2) { // 確保不超出邊界並且左邊是空地
                             m_GameMap[i][j] = 2; // 將當前位置設為空地
                             m_GameMap[i][j - 1] = 4; // 將左邊一格設為玩家
-                            m_Player->SetPosition({ m_Player->GetPosition().x - 60, m_Player->GetPosition().y });
+                            m_Player->SetPosition({ m_Player->GetPosition().x - 40, m_Player->GetPosition().y });
                             needUpdate = true;
                         }
-                        if (j > 0 && m_GameMap[i][j - 1] == 3) {
-                            if (j > 1 && m_GameMap[i][j - 2] == 2) { // 確保箱子不會超出邊界並且箱子左邊是空地
+                        if (m_GameMap[i][j - 1] == 3) {
+                            if (m_GameMap[i][j - 2] == 2) { // 確保箱子不會超出邊界並且箱子左邊是空地
                                 m_GameMap[i][j] = 2; // 將當前位置設為空地
                                 m_GameMap[i][j - 1] = 4; // 將左邊一格設為玩家
                                 m_GameMap[i][j - 2] = 3; // 將箱子左邊一格設為箱子
-                                m_Player->SetPosition({ m_Player->GetPosition().x - 60, m_Player->GetPosition().y });
-                                m_Box->SetPosition({ m_Box->GetPosition().x - 60, m_Box->GetPosition().y });
+                                m_Player->SetPosition({ m_Player->GetPosition().x - 40, m_Player->GetPosition().y });
+                                m_Box->SetPosition({ m_Box->GetPosition().x - 40, m_Box->GetPosition().y });
                                 needUpdate = true;
                             }
                         }
@@ -116,21 +120,21 @@ void App::Update() {
     else if (Util::Input::IsKeyPressed(Util::Keycode::RIGHT)) {
         if (!keyProcessed) {
             for (int i = 7; i >= 0; i--) {
-                for (int j = 0; j < 6; j++) {
+                for (int j = 0; j < 9; j++) {
                     if (m_GameMap[i][j] == 4) {
-                        if (j < 5 && m_GameMap[i][j + 1] == 2) { // 確保不超出邊界並且右邊是空地
+                        if (m_GameMap[i][j + 1] == 2) { // 確保不超出邊界並且右邊是空地
                             m_GameMap[i][j] = 2; // 將當前位置設為空地
                             m_GameMap[i][j + 1] = 4; // 將右邊一格設為玩家
-                            m_Player->SetPosition({ m_Player->GetPosition().x + 60, m_Player->GetPosition().y });
+                            m_Player->SetPosition({ m_Player->GetPosition().x + 40, m_Player->GetPosition().y });
                             needUpdate = true;
                         }
-                        if (j < 5 && m_GameMap[i][j + 1] == 3) {
-							if (j < 4 && m_GameMap[i][j + 2] == 2) { // 確保箱子不會超出邊界並且箱子右邊是空地
+                        if (m_GameMap[i][j + 1] == 3) {
+							if (m_GameMap[i][j + 2] == 2) { // 確保箱子不會超出邊界並且箱子右邊是空地
 								m_GameMap[i][j] = 2; // 將當前位置設為空地
 								m_GameMap[i][j + 1] = 4; // 將右邊一格設為玩家
 								m_GameMap[i][j + 2] = 3; // 將箱子右邊一格設為箱子
-								m_Player->SetPosition({ m_Player->GetPosition().x + 60, m_Player->GetPosition().y });
-								m_Box->SetPosition({ m_Box->GetPosition().x + 60, m_Box->GetPosition().y });
+								m_Player->SetPosition({ m_Player->GetPosition().x + 40, m_Player->GetPosition().y });
+								m_Box->SetPosition({ m_Box->GetPosition().x + 40, m_Box->GetPosition().y });
 								needUpdate = true;
 							}
                         }
--- src/InitializeMap.cpp
+++ src/InitializeMap.cpp
@@ -28,19 +28,20 @@ void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
 	m_GameMap.assign(GameMap.begin(), GameMap.end());
 
     // 原點在中間，所以左下角是(-180, -320)
-    // 640-480=160，160/2=80，所以上下各空80
-    // 第一個(左下角)箱子: (-150, -210)
+    // 640-320=320，320/2=160，所以上下各空160
+    // 第一個(左下角)箱子: (-160, -140)
     // SetPosition是設定中間的位置
 
     for (int i = 7; i >= 0; i--) {
-        for (int j = 0; j < 6; j++) {
+        for (int j = 0; j < 9; j++) {
             int tile = GameMap[i][j];
             switch (tile) {
                 case 1: // 牆壁
                 {
+					/* 牆壁 */
                     auto wall = std::make_shared<Character>(RESOURCE_DIR"/Object/wall.png");
                     wall->SetZIndex(50);
-                    wall->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    wall->SetPosition({ -160 + 40 * j, 140 - 40 * i });
                     wall->SetVisible(false);
                     m_Wall.push_back(wall);
                     m_Root.AddChild(wall);
@@ -48,9 +49,10 @@ void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
                     break;
                 case 2: // 空地
                 {
+                    /* 地板 */
                     auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
-                    floor->SetZIndex(40);
-                    floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    floor->SetZIndex(30);
+                    floor->SetPosition({ -160 + 40 * j, 140 - 40 * i });
                     floor->SetVisible(false);
                     m_Floor.push_back(floor);
                     m_Root.AddChild(floor);
@@ -58,15 +60,17 @@ void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
                     break;
                 case 3: // 箱子
                 {
+                    /* 地板 */
                     auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
-                    floor->SetZIndex(40);
-                    floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    floor->SetZIndex(30);
+                    floor->SetPosition({ -160 + 40 * j, 140 - 40 * i });
                     floor->SetVisible(false);
                     m_Floor.push_back(floor);
                     m_Root.AddChild(floor);
 
+                    /* 箱子 */
                     m_Box = std::make_shared<Character>(RESOURCE_DIR"/Object/box.png");
-                    m_Box->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    m_Box->SetPosition({ -160 + 40 * j, 140 - 40 * i });
                     m_Box->SetVisible(false);
                     m_Box->SetZIndex(50);
                     m_Root.AddChild(m_Box);
@@ -74,20 +78,54 @@ void App::InitializeMap(const std::vector<std::vector<int>>& GameMap) {
                     break;
                 case 4: // 人物
                 {
+                    /* 地板 */
                     auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
-                    floor->SetZIndex(40);
-                    floor->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    floor->SetZIndex(30);
+                    floor->SetPosition({ -160 + 40 * j, 140 - 40 * i });
                     floor->SetVisible(false);
                     m_Floor.push_back(floor);
                     m_Root.AddChild(floor);
 
+                    /* 人物 */
                     m_Player = std::make_shared<Character>(RESOURCE_DIR"/Object/player_down.png");
-                    m_Player->SetPosition({ -150 + 60 * j, 210 - 60 * i });
+                    m_Player->SetPosition({ -160 + 40 * j, 140 - 40 * i });
                     m_Player->SetVisible(false);
                     m_Player->SetZIndex(50);
                     m_Root.AddChild(m_Player);
                 }
                     break;
+                case 5: // 目標點
+                {
+                    /* 地板 */
+                    auto floor = std::make_shared<Character>(RESOURCE_DIR"/Object/floor.png");
+                    floor->SetZIndex(30);
+                    floor->SetPosition({ -160 + 40 * j, 140 - 40 * i });
+                    floor->SetVisible(false);
+                    m_Floor.push_back(floor);
+                    m_Root.AddChild(floor);
+
+                    /* 人物 */
+                    m_Player = std::make_shared<Character>(RESOURCE_DIR"/Object/player_down.png");
+                    m_Player->SetPosition({ -160 + 40 * j, 140 - 40 * i });
+                    m_Player->SetVisible(false);
+                    m_Player->SetZIndex(50);
+                    m_Root.AddChild(m_Player);
+
+                    /* 目標點 */
+					m_Point = std::make_shared<Character>(RESOURCE_DIR"/Object/point.png");
+                    m_Point->SetPosition({ -160 + 40 * j, 140 - 40 * i });
+                    m_Point->SetVisible(false);
+                    m_Point->SetZIndex(40);
+                    m_Root.AddChild(m_Point);
+
+                    /* 勾勾 */
+					m_Check = std::make_shared<Character>(RESOURCE_DIR"/Object/check.png");
+                    m_Check->SetPosition({ -160 + 40 * j, 140 - 40 * i });
+                    m_Check->SetVisible(false);
+                    m_Check->SetZIndex(60);
+                    m_Root.AddChild(m_Check);
+				}
+				    break;
                 default:
                     break;
             }
